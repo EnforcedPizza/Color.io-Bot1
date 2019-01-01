de81888a64ee3ae27a21904b24936e83f3e97a12
EnforcedPizza <46079606+EnforcedPizza@users.noreply.github.com>
2018-12-31 23:41:55 -0700

    Update generate_simple_embed.js

--- generate_simple_embed.js
+++ generate_simple_embed.js
@@ -6,7 +6,7 @@ module.exports = async function generate_simple_embed(name, value, color = Math.
                     "color": color,
                     "timestamp": "2018-12-23T05:19:10.633Z",
                     "footer": {
-                        "text": "Color.io | @The3DSquare"
+                        "text": "Color.io | @CallMehMiles"
                     },
                     "fields": [
                         {
